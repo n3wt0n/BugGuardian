d5dc7e4741b1dabed980db80773805391eee5dc9
Davide Benvegnù <davide.benvegnu@outlook.com>
2017-02-24 22:13:58 +0800

    Changed Assembly information

--- BugGuardian.NetStandard/Properties/AssemblyInfo.cs
+++ BugGuardian.NetStandard/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BugGuardian.NetStandard")]
+[assembly: AssemblyTitle("DBTek.BugGuardian")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BugGuardian.NetStandard")]
+[assembly: AssemblyCompany("DBTek")]
+[assembly: AssemblyProduct("DBTek.BugGuardian")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
